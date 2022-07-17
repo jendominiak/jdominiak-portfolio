9abc4835a053216ba09dddeab3597bc95806364d
Angela Hanshaw <angela.hanshaw@gmail.com>
2022-07-16 23:58:10 -0500

    add progress

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,26 @@ const sourcemaps = require("gulp-sourcemaps");
 const jsonToSass = require("gulp-json-data-to-sass");
 
 function jsonCss() {
-  return src("./stylevariables.json").pipe(
-    jsonToSass({
-      sass: "./scss/stylevariables.scss",
-      separator: "",
-    })
-  );
+	return src("./stylevariables.json").pipe(
+		jsonToSass({
+			sass: "./scss/stylevariables.scss",
+			separator: "",
+		})
+	);
 }
 function cssTask() {
-  return src("./scss/*.scss", { allowEmpty: true })
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: "compressed" }))
-    .on("error", sass.logError)
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("./styles"));
+	return src("./scss/*.scss", { allowEmpty: true })
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: "compressed" }))
+		.on("error", sass.logError)
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./styles"));
 }
 
 function watchFiles() {
-  watch("./scss/*.scss", parallel(cssTask));
-  watch("./stylevariables.json", parallel(jsonCss));
+	watch("./scss/**", parallel(cssTask));
+	watch("./stylevariables.json", parallel(jsonCss));
 }
 
 exports.build = series(jsonCss, cssTask);
